f44748d685176ff78f0879424883d1d30ebd9967
poliniesta <poingo@fp.insjoaquimmir.cat>
2023-05-09 12:24:06 +0200

    Acabada ContentInfo Page

--- DangerHubReact/src/components/aplicacio/Info.jsx
+++ DangerHubReact/src/components/aplicacio/Info.jsx
@@ -1,36 +1,92 @@
-import React, { useState } from 'react';
-import { PopUp } from './PopUp'
-import Scream from '/imagenes/scream.png';
+import React, { useEffect, useState } from 'react';
+import laMonja from '/imagenes/laMonja.jpg';
+import noRespires from '/imagenes/noRespires.jpg';
+import Saw from '/imagenes/Saw.jpg';
+import Saw4 from '/imagenes/Saw4.jpg';
+import Anabelle from '/imagenes/Anabelle.jpg';
+
 import { BsPlay } from 'react-icons/bs';
+import { MdBookmarkAdd } from 'react-icons/md';
+import { AiOutlineCloudDownload } from 'react-icons/ai';
 import { BsFillArrowRightCircleFill } from 'react-icons/bs';
 import { BsFillArrowLeftCircleFill } from 'react-icons/bs';
-import { AiOutlineInfoCircle } from 'react-icons/ai';
 import { Link } from "react-router-dom";
 
 export const Info = () => {
 
+  const [descriptionVisible, setDescriptionVisible] = useState(true);
 
+  useEffect(() => {
+
+    const container1 = document.querySelector('.films');
+    const leftArrow1 = document.querySelector('.carousel-arrow.left-info');
+    const rightArrow1 = document.querySelector('.carousel-arrow.right-info');
+
+    leftArrow1.addEventListener('click', () => {
+      container1.scrollBy({ left: -1000, behavior: 'smooth' });
+    });
+
+    rightArrow1.addEventListener('click', () => {
+      container1.scrollBy({ left: 1000, behavior: 'smooth' });
+    });
+
+    document.getElementById("description").style.display = "block";
+
+    setDescriptionVisible(true);
+
+  }, []);
+
+  // Open the default tab on page load
   
+
+  // Switch between tabs when a tab button is clicked
+  function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    setDescriptionVisible(tabName === "description");
+  }
+
   return (
     <>
 
       <div className="container-info">
         <div className="portada-info">
-          <img src={Scream}/>
+          <img src={laMonja}/>
         </div>
+
         <div className="content-box-info">
-          <div className="title">Titulo Pelicula</div>
+          <div className="title">La Monja</div>
+
           <div className="details">
-            <div>2h 23min</div>
-            <div>|</div>
             <div>2022</div>
+            <div>|</div>
+            <div>2h 23min</div>
             <div>|</div>
             <div>+18</div>
           </div>
+
           <div className="tabs">
-            <div className="general">GENERAL</div> 
+            <div className="tablink active" onClick={(e) => openTab(e, 'description')}>GENERAL</div>
+            <div className="tablink" onClick={(e) => openTab(e, 'opciones')}>MÁS OPCIONES</div> 
           </div> 
-          <div className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eum, error voluptatem ad perferendis dignissimos veritatis, et est ut vero totam consequatur sit quisquam velit, commodi beatae! Libero, officiis ipsa.</div>
+          <div id="description" className="tabcontent description" style={{display: descriptionVisible ? "flex" : "none"}}>
+          Tras la muerte de una joven monja en un convento de Rumanía El Vaticano envía para realizar una investigación a una monja a punto tomar los votos y a un cura experto en posesiones. Una terrible confrontación entre el mundo de los vivos y el de los muertos se producirá con su llegada al convento.
+          </div>
+          <div id="opciones" className="tabcontent opciones" style={{display: descriptionVisible ? "none" : "flex", width: "250px"}}>
+            <div><BsPlay/></div>
+            <div><MdBookmarkAdd/></div>
+            <div><AiOutlineCloudDownload/></div>
+          </div>
           
           <div className="specific-info-box">
             <div className="specific-title">Autor:</div>
@@ -38,28 +94,46 @@ export const Info = () => {
           </div> 
 
           <div className="related_films">
-           <div className="title">
-              Peliculas Relacionadas
-            </div>
+            <div className="title">Peliculas Relacionadas</div>
+            <div className="carousel-arrow left-info"><BsFillArrowLeftCircleFill/></div>
+            <div className="carousel-arrow right-info"><BsFillArrowRightCircleFill/></div>
+
             <div className="films">
               <div className="film">
-                <img src={Scream} alt="" />
+                <img src={noRespires} alt="" />
               </div>
 
               <div className="film">
-                <img src={Scream} alt="" />
+                <img src={Saw} alt="" />
               </div>
               
               <div className="film">
-                <img src={Scream} alt="" />
+                <img src={Saw4} alt="" />
               </div>
+          
+              <div className="film">
+                  <img src={Anabelle} alt="" />
+              </div>
+
+              <div className="film">
+                <img src={noRespires} alt="" />
+              </div>
+
+              <div className="film">
+                <img src={Saw} alt="" />
+              </div>
+
+              <div className="film">
+                <img src={Saw4} alt="" />
+              </div>
+
+              <div className="film">
+                <img src={Anabelle} alt="" />
+              </div>
+              
             </div>
-            
           </div>
 
-          <div className="buttons">
-            <Link to="/play"><div className="button-play"><BsPlay/>Play</div></Link>
-          </div>
         </div>
       </div>
               
